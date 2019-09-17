d730d9a0b7e0e805bed16cca4057bdf3cb4da374
WL Developer <dev00@SynopticProject.local>
2019-09-17 15:38:56 +0100

    hi

---
+++ database/migrations/2019_09_17_140634_create_media_database.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMediaDatabase extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create( 'media_database', function ( Blueprint $table ) {
+            $table->bigIncrements( 'id' );
+            $table->string( 'name' );
+            $table->string( 'artist' );
+            $table->string( 'album' );
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('media_database');
+    }
+}
