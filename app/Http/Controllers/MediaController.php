88ed1f0159904273cce13633cb5e9436205f3e28
WL Developer <dev00@SynopticProject.local>
2019-12-16 12:38:29 +0000

    ***UNdo this to work***

--- app/Http/Controllers/MediaController.php
+++ app/Http/Controllers/MediaController.php
@@ -9,16 +9,25 @@ use App\MediaModel;
 class MediaController extends Controller
 {
     public function GetMediaData( Request $aRequest ){
-      $lData = MediaModel::all();
-      return view( 'welcome', $lData );
+        $lData = MediaModel::all();
+        return view( 'welcome', $lData );
 
     }
 
     public function LoadMediaData( Request $aRequest ){
-      return MediaModel::all();
+        return MediaModel::all();
     }
 
     public function DeleteMediaEntry( Request $aRequest ){
-      return "hello";
+        $lInput = $aRequest->all();
+        /* Make sure we have got our payload - i.e. "IDsToDelete": array( 1,2,3,4 ) */
+        
+
+        /* Is the payload valid - check if its an array, and that the information inside is what you want */
+
+        /* If using IDs, look at doing a delete using laravel ORM delete */
+
+        $lUserID = $this->argument( 'id' );
+        return view( 'welcome', $lData );
     }
 }
--- resources/js/adminClass.js
+++ resources/js/adminClass.js
@@ -1,68 +1,63 @@
 class Admin {
-  static GetMediaData(){
-    axios.get( '/api/MediaData' ).then( function ( aData ) {
-      let lTable = $( '#media-datatable' ).DataTable({
-        data: aData.data,
-        searching: true,
-        filtering: true,
-        orderCellsTop: true,
-        fixedHeader: true,
-        searchable: true,
-        select: {
-          style: 'multi'
-        },
-        multiselect: true,
-        responsive: true,
-        ordering: true,
-        pagingType: "simple_numbers",
-        columns: [
-          {
-            title: 'Name',
-            data: null,
-            render: function( data ){
-              return data.name;
-            }
-          },
-          {
-            title: 'Artist',
-            data: null,
-            render: function( data ){
-              return data.artist;
-            }
-          },
-          {
-            title: 'Album',
-            data: null,
-            render: function( data ){
-              return data.album;
-            }
-          }
-        ]
-      });
-    }).catch( function( aError ) {
-      console.log( aError );
-    });
-  }
-
-  static DeleteMediaEntry(){
-    console.log("Delete button has been clicked");
-    let lTable = $( '#media-datatable' ).DataTable();
-    let lGeorgia = lTable.rows( '.selected' ).data();
-    // console.log(lGeorgia[1]);
-    let i;
-    for ( i = 0; i < lGeorgia.length; i++ ){
-      console.log(lGeorgia[i]);
-      let lName = lGeorgia[ i ].name;
-      console.log(lName);
-      axios.delete( '/api/MediaData', { data: { "name": lName } } ).then( function( aData ){
-        this.GetMediaData();
-        console.log("Weve deleted it boys");
-      }).catch( function( aError ){
-        console.log( aError );
-      });
+    static GetMediaData(){
+        axios.get( '/api/MediaData' ).then( function ( aData ) {
+            let lTable = $( '#media-datatable' ).DataTable({
+                data: aData.data,
+                searching: true,
+                filtering: true,
+                orderCellsTop: true,
+                fixedHeader: true,
+                searchable: true,
+                select: {
+                    style: 'multi'
+                },
+                multiselect: true,
+                responsive: true,
+                ordering: true,
+                pagingType: "simple_numbers",
+                columns: [
+                    {
+                        title: 'Name',
+                        data: null,
+                        render: function( data ){
+                            return data.name;
+                        }
+                    },
+                    {
+                        title: 'Artist',
+                        data: null,
+                        render: function( data ){
+                            return data.artist;
+                        }
+                    },
+                    {
+                        title: 'Album',
+                        data: null,
+                        render: function( data ){
+                            return data.album;
+                        }
+                    }
+                ]
+            });
+        }).catch( function( aError ) {
+            console.log( aError );
+        });
     }
-    // let lSelectedValue = $( "#media-datatable" ).DataTable().row( { selected: true } ).data().name;
-    // console.log( lSelectedValue );
 
-  }
+    static DeleteMediaEntry( ){
+        let lThis = this;
+        console.log("Delete button has been clicked");
+        let lTable = $( '#media-datatable' ).DataTable();
+        let lGeorgia = lTable.rows( '.selected' ).data();
+        let lPayload = [];
+        for ( let i = 0; i < lGeorgia.length; i++ ){
+            let lSelected = lGeorgia[ i ].id;
+            lPayload.push( lSelected );
+        }
+        let lGeorgia1 = JSON.stringify( lPayload );
+        console.log(lGeorgia);
+
+        // console.log(lSelectedArray);
+        // let lSelectedValue = $( "#media-datatable" ).DataTable().row( { selected: true } ).data().name;
+    }
 }
