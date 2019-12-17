2ba2e760e8dd3bfd5bc72a5ea372a4948eda7105
WL Developer <dev00@SynopticProject.local>
2019-12-17 17:01:37 +0000

    /**/ adding notes

--- app/Http/Controllers/MediaController.php
+++ app/Http/Controllers/MediaController.php
@@ -20,17 +20,32 @@ class MediaController extends Controller
 
     public function DeleteMediaEntry( Request $aRequest ){
         $lInput = $aRequest->all();
-
+        // dd( $lInput );
         /* Is the payload valid - check if its an array, and that the information inside is what you want */
         if ( !array_key_exists( "ID", $lInput ) ){
-            /* there was no ID provided */
-            
+            /* there was no ID provided -> log error */
+            // dd( "No ID provided" );
         }
+
+        $lGeorgia = MediaModel::all();
+        /* VALIDATE :: make sure that the selected ID is present in the database */
+        dd( $lGeorgia);
+        /* find a way to get error messages */
+
+
+        // if( $lInput[ "ID" ] in  )
+
+
+
+        // if ( count( $lInput ) < 1 ){
+        //     dd( "im an empty array" );
+        //
+        // }
 
         /* If using IDs, look at doing a delete using laravel ORM delete */
         $lID = MediaModel::find( $lInput[ "ID" ] );
         $lID->delete();
-
+        /* Log the successful delete */
         return response()->json([
             "ReturnStatus" => 'Success',
             "ReturnCode" => 200,
